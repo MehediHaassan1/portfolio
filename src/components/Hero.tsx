cb85c91dc45b49f5f54eb4e5613e05bdd45fd431
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-09-06 22:12:25 +0600

    basic setup done & navbar and hero section done

---
+++ src/components/Hero.tsx
@@ -0,0 +1,122 @@
+import bannerImage from "../assets/banner-image.svg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram, FaFacebook } from "react-icons/fa6";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+    const iconRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (iconRef.current) {
+            const icons: NodeListOf<HTMLAnchorElement> =
+                iconRef.current.querySelectorAll("a");
+
+            icons.forEach((icon: HTMLAnchorElement) => {
+                const magneticEffect = (e: MouseEvent) => {
+                    const boundingRect = icon.getBoundingClientRect();
+                    const iconCenterX =
+                        boundingRect.left + boundingRect.width / 2;
+                    const iconCenterY =
+                        boundingRect.top + boundingRect.height / 2;
+                    const distX = (e.clientX - iconCenterX) / 2; // Adjust effect strength
+                    const distY = (e.clientY - iconCenterY) / 2;
+
+                    gsap.to(icon, {
+                        x: distX,
+                        y: distY,
+                        duration: 0.4,
+                        ease: "power3.out",
+                    });
+                };
+
+                const resetPosition = () => {
+                    gsap.to(icon, {
+                        x: 0,
+                        y: 0,
+                        duration: 0.6,
+                        ease: "power3.out",
+                    });
+                };
+
+                icon.addEventListener("mousemove", magneticEffect);
+                icon.addEventListener("mouseleave", resetPosition);
+
+                // Clean up event listeners on unmount
+                return () => {
+                    icon.removeEventListener("mousemove", magneticEffect);
+                    icon.removeEventListener("mouseleave", resetPosition);
+                };
+            });
+        }
+    }, []);
+
+    return (
+        <div className="container mx-auto min-h-screen">
+            <div className="min-h-screen w-full flex flex-col gap-2 md:flex-row md:items-center md:justify-between">
+                <div className="h-full w-full md:w-1/2 order-last md:order-first">
+                    <h1 className="text-[28px] md:text-[24px] lg:text-[36px]">
+                        Hello I'am{" "}
+                        <span className="font-bold">Mehedi Hassan</span>
+                    </h1>
+                    <h1 className="text-[32px] md:text-[24px] lg:text-[40px]">
+                        MERN Stack{" "}
+                        <span className="font-bold outline-text">
+                            Developer
+                        </span>
+                    </h1>
+                    <h1 className="text-[36px] md:text-[28px] lg:text-[44px] font-bold">
+                        Bangladesh 2.0
+                    </h1>
+                    <p className="text-[16px] lg:text-[18px] my-4 lg:my-10 text-zinc-600">
+                        I am Mehedi Hassan, a passionate MERN stack developer
+                        with a knack for creating dynamic, full-stack web
+                        applications. My expertise spans from building seamless
+                        user interfaces to managing robust backend services. I'm
+                        dedicated to leveraging the latest technologies to
+                        deliver high-quality solutions.
+                    </p>
+                    <div className="flex items-center gap-8" ref={iconRef}>
+                        <a
+                            href="https://github.com"
+                            target="_blank"
+                            className="size-12 flex items-center justify-center rounded"
+                        >
+                            <FaGithub className="size-8 text-black" />
+                        </a>
+                        <a
+                            href="https://linkedin.com"
+                            target="_blank"
+                            className="size-12 flex items-center justify-center rounded "
+                        >
+                            <FaLinkedin className="size-8 text-black" />
+                        </a>
+                        <a
+                            href="https://instagram.com"
+                            target="_blank"
+                            className="size-12 flex items-center justify-center rounded "
+                        >
+                            <FaSquareInstagram className="size-8 text-black" />
+                        </a>
+                        <a
+                            href="https://facebook.com"
+                            target="_blank"
+                            className="size-12 flex items-center justify-center rounded "
+                        >
+                            <FaFacebook className="size-8 text-black" />
+                        </a>
+                    </div>
+                </div>
+                <div className="w-4/5 mx-auto md:mx-0 md:w-1/2 border-black border-b-2 md:flex items-center justify-center">
+                    <img
+                        src={bannerImage}
+                        alt="banner"
+                        className="h-full w-full lg:w-4/5"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,139 @@
+import { useRef, useState } from "react";
+import logo from "../assets/main-logo.svg";
+import { Cross as Hamburger } from "hamburger-react";
+import { FiDownload } from "react-icons/fi";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Navbar = () => {
+    const [isOpen, setOpen] = useState(false);
+    const smContainer = useRef<HTMLDivElement | null>(null);
+    const textRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+
+    const addToTextRefs = (el: HTMLAnchorElement | null) => {
+        if (el && !textRefs.current.includes(el)) {
+            textRefs.current.push(el);
+        }
+    };
+
+    const navigationMenu = [
+        { path: "#about-me", title: "About Me" },
+        { path: "#skills", title: "Skills" },
+        { path: "#projects", title: "Projects" },
+        { path: "#contact-me", title: "Contact Me" },
+    ];
+
+    useGSAP(() => {
+        if (!smContainer.current) return;
+
+        if (isOpen) {
+            gsap.set(smContainer.current, {
+                y: "-100%",
+                height: 0,
+                opacity: 0,
+            });
+            gsap.to(smContainer.current, {
+                y: 0,
+                height: "calc(100vh - 3rem)",
+                opacity: 1,
+                duration: 0.6,
+                ease: "power3.out",
+                onComplete: () => {
+                    gsap.fromTo(
+                        textRefs.current,
+                        { opacity: 0, y: -40 },
+                        {
+                            opacity: 1,
+                            y: 0,
+                            duration: 0.6,
+                            ease: "power3.out",
+                            stagger: 0.2,
+                        }
+                    );
+                },
+            });
+        } else {
+            gsap.to(textRefs.current, {
+                opacity: 0,
+                y: -40,
+                duration: 0.6,
+                ease: "power3.in",
+                stagger: 0.1,
+                onComplete: () => {
+                    gsap.to(smContainer.current, {
+                        y: "0",
+                        height: 0,
+                        opacity: 0,
+                        duration: 0.6,
+                        ease: "power3.in",
+                    });
+                },
+            });
+        }
+    }, [isOpen]);
+
+    return (
+        <div className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+            <div className="container mx-auto relative">
+                <div className="h-12 lg:h-20 w-full flex items-center justify-between ">
+                    <div>
+                        <img src={logo} alt="M." className="size-12" />
+                    </div>
+
+                    {/* large device */}
+                    <div className="md:flex items-center md:gap-3 lg:gap-5 hidden">
+                        {navigationMenu.map((item, idx) => (
+                            <a
+                                className="md:px-3 md:py-2 px-5 py-3 text-black font-semibold md:text-[18px] text-[20px]"
+                                href={item.path}
+                                key={idx}
+                            >
+                                {item.title}
+                            </a>
+                        ))}
+                    </div>
+
+                    <div className="flex items-center gap-4">
+                        <button className="flex items-center gap-2 text-md lg:text-xl px-3 py-2 lg:px-5 lg:py-4 bg-black text-white rounded">
+                            Resume <FiDownload />
+                        </button>
+                        <div className="md:hidden">
+                            <Hamburger
+                                toggled={isOpen}
+                                toggle={setOpen}
+                                size={32}
+                                duration={0.8}
+                            />
+                        </div>
+                    </div>
+                </div>
+                {/* small & medium device */}
+                <div
+                    ref={smContainer}
+                    className="absolute left-0 right-0 bg-black flex items-center justify-center md:hidden"
+                    style={{
+                        top: "3rem",
+                        bottom: 0,
+                        overflow: "hidden",
+                    }}
+                >
+                    <div className="flex flex-col gap-4 items-center justify-center px-4 py-8 text-white space-y-5">
+                        {navigationMenu.map((item, idx) => (
+                            <div key={idx} className="overflow-hidden">
+                                <a
+                                    ref={addToTextRefs}
+                                    href={item.path}
+                                    className="h-fit bg-black text-2xl p-0 block"
+                                >
+                                    {item.title}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
